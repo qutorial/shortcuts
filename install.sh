a66ea8c8681807d9a96a2e82079920f33721a182
Zaur Molotnikov <qutorial@gmail.com>
2018-05-09 17:43:27 +0200

    Install

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-# KDE shortcuts
-
-Put these files into ~/.config and enjoy!
-
-```
 curl https://raw.githubusercontent.com/qutorial/shortcuts/master/.bash_aliases > ~/.bash_aliases
 curl https://raw.githubusercontent.com/qutorial/shortcuts/master/networking.sh > ~/networking.sh
 curl https://raw.githubusercontent.com/qutorial/shortcuts/master/kcminputrc > ~/.config/kcminputrc
@@ -13,4 +8,5 @@ curl https://raw.githubusercontent.com/qutorial/shortcuts/master/plasma-localerc
 curl https://raw.githubusercontent.com/qutorial/shortcuts/master/powermanagementprofilesrc > ~/.config/powermanagementprofilesrc
 curl https://raw.githubusercontent.com/qutorial/shortcuts/master/plasma-org.kde.plasma.desktop-appletsrc > ~/.config/plasma-org.kde.plasma.desktop-appletsrc
 echo "source ~/.bash_aliases" >> ~/.bashrc
-```
+souce ~/.bashrc
+
