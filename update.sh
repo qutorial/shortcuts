b6aa3a90a8734655eb36f1eb850bdc5c58fea648
Zaur Molotnikov <qutorial@gmail.com>
2018-05-09 17:41:41 +0200

    Before the updated

---
+++ update.sh
@@ -0,0 +1,10 @@
+cp ~/.bash_aliases ./
+cp ~/.config/kcminputrc ./
+cp ~/.config/kglobalshortcutsrc ./
+cp ~/.config/khotkeysrc ./
+cp ~/.config/kxkbrc ./
+cp ~/.config/plasma-localerc ./
+cp ~/.config/powermanagementprofilesrc ./
+cp ~/.config/plasma-org.kde.plasma.desktop-appletsrc ./
+cp ~/networking.sh ./
+
